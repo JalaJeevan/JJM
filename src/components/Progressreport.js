d3fdfd970dcd12b2710a670bb226954a70deaf00
JalaJeevan <121668200+JalaJeevan@users.noreply.github.com>
2023-01-02 16:33:33 +0530

    Update Progressreport.js

--- src/components/Progressreport.js
+++ src/components/Progressreport.js
@@ -1,27 +1,121 @@
-import './Staffing.css'
+import './Progressreport.css'
 
 export const Progressreport = () => {
   return  (   
-  <><div class="container1">
-      <div class="header">
-        <h3>Staff Details</h3>
-      </div>
+  <>   <div class="container">
+  <div class="header">
+    <h3>District   </h3>
+  </div>
+</div> <div class="container10">
+  <div class="row">
+  <div class="col-25">
+  <label for="subject">District Name: </label>
+   </div>
+    <div class="col-75">
+    <select>
+    <option value="Dharwad">Dharwad</option>
+    <option value="Haveri">Haveri</option>
+    <option value="Gadag">Gadag</option>
+        </select>
+   </div>
+   </div>
+   </div> 
+
+   <div class="container11">
+  <div class="header">
+    <h3> Administrative Details   </h3>
+  </div>
+</div> <div class="container12">
+  <div class="row">
+  <div class="col-25">
+  <label for="subject">Panchayat Name: </label>
+   </div>
+   <div class="col-75">
+      <input type="text" id="subject" name="subject" placeholder="Enter the Panchayat Name" />
     </div>
-    <div class="container2">
-      <div class="header">
-        <h3>Attendance</h3>
-      </div>
+   </div>
+   </div> <br/>
+
+   <div class="container13">
+  <div class="row">
+  <div class="col-25">
+  <label for="subject">Gram Panchayat: </label>
+   </div>
+   <div class="col-75">
+      <input type="text" id="subject" name="subject" placeholder="Enter the Gram Panchayat Name" />
     </div>
-    <div class="container3">
-      <div class="header">
-        <h3>Diary</h3>
-      </div>
+   </div>
+   </div> <br/>
+  
+   <div class="container14">
+  <div class="row">
+  <div class="col-25">
+  <label for="subject">Block: </label>
+   </div>
+   <div class="col-75">
+      <input type="text" id="subject" name="subject" placeholder="Enter the Block:" />
     </div>
-    <div class="container4">
-      <div class="header">
-        <h3>Reports</h3>
-      </div>
+   </div>
+   </div> <br/>
+
+   <div class="container15">
+  <div class="row">
+  <div class="col-25">
+  <label for="subject">District: </label>
+   </div>
+   <div class="col-75">
+      <input type="text" id="subject" name="subject" placeholder="Enter the District:" />
     </div>
+   </div>
+   </div> <br/>
+
+   <div class="container11">
+  <div class="header">
+    <h3> Administrative Details   </h3>
+  </div>
+</div> <div class="container12">
+  <div class="row">
+  <div class="col-25">
+  <label for="subject">Panchayat Name: </label>
+   </div>
+   <div class="col-75">
+      <input type="text" id="subject" name="subject" placeholder="Enter the Panchayat Name" />
+    </div>
+   </div>
+   </div> <br/>
+
+   <div class="container13">
+  <div class="row">
+  <div class="col-25">
+  <label for="subject">Gram Panchayat: </label>
+   </div>
+   <div class="col-75">
+      <input type="text" id="subject" name="subject" placeholder="Enter the Gram Panchayat Name" />
+    </div>
+   </div>
+   </div> <br/>
+  
+   <div class="container14">
+  <div class="row">
+  <div class="col-25">
+  <label for="subject">Block: </label>
+   </div>
+   <div class="col-75">
+      <input type="text" id="subject" name="subject" placeholder="Enter the Block:" />
+    </div>
+   </div>
+   </div> <br/>
+
+   <div class="container15">
+  <div class="row">
+  <div class="col-25">
+  <label for="subject">District: </label>
+   </div>
+   <div class="col-75">
+      <input type="text" id="subject" name="subject" placeholder="Enter the District:" />
+    </div>
+   </div>
+   </div> <br/>
     </>
   )
-  }
+}
